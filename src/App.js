c0869632fe741263803b8594471404b8eb512184
ShahidUllahSE <shahidbangash8899@gmail.com>
2024-04-18 13:40:23 +0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './SignIn';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+<Router>
+
+<Routes>
+
+
+<Route path='/' element={<Home/>} />
+<Route path='/Sign-in'element={<SignIn/>} />
+<Route path='/Sign-up' element={<SignUp/>} />
+<Route path='/About' element={<About/>} />
+<Route path='/Profile' element={<Profile/>} />
+
+</Routes>
+
+
+</Router>
+
+  </div>
   );
 }
 
